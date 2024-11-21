36ab6243fd5411d7da8c0b7b7908bd669efa3fd7
김동욱 <69969001+dongwook1214@users.noreply.github.com>
2024-11-21 22:51:21 +0900

    feat GetAllTransactions #9

--- fabric-network/test_sh.sh
+++ fabric-network/test_sh.sh
@@ -25,8 +25,17 @@ export CORE_PEER_ADDRESS=localhost:7051
 # 트랜젝션 전송 alice to bob
 # peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C mychannel -n matter-tunnel --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" -c '{"function":"Queuing","Args":["04992a9a2eb063fc4cd45c5b67a1aecebcb87ea8358de73eef0ccf7d58c3c3ff7e382661df97cdfa48fede1ad2b767b95268bc810e11637234bb353085e37c32c7","d865063d1cb8870d2130e1780190838875d4a49ad17f405c9f6d78035e3df38fed84368ddbd394a7293240f6508306d079363620e8bbb47a105b6a6f4c001e2868696869000000000000000000000000000003750bfae2e57e7160cb5ead399ab37afdb4a1451a0b96b08764296dbe8490d946bf0f3f670000000090968c3bc13aa16a6409ccb7af4f29b4e2dca13718fda9cc21f5eb5db606a163"]}'
 
+# 트랜젝션 전송 bob to alice
+# peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C mychannel -n matter-tunnel --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" -c '{"function":"Queuing","Args":["04750bfae2e57e7160cb5ead399ab37afdb4a1451a0b96b08764296dbe8490d946f1312034836474ccf7070b44d3e98f03dca538d148aff42fce155f58243de60d","b59ab4438f680ee60b712e0e6b23ba82137bd549fe8163ba0cd32b07dc5d682eeb7c3499b5194a3eccc6ae7caac299d42b1a0e8639f12e58b5a444ecd8e2feaa68696869000000000000000000000000000003750bfae2e57e7160cb5ead399ab37afdb4a1451a0b96b08764296dbe8490d946dd203f670000000001eb4ed987665707f37aa836539010f9388a9c6ed9bc3b39207f4045d5fff748"]}'
+
 # 트랜젝션 확인 bob
 # peer chaincode query -C mychannel -n matter-tunnel -c '{"function":"GetCurrentTransaction","Args":["04992a9a2eb063fc4cd45c5b67a1aecebcb87ea8358de73eef0ccf7d58c3c3ff7e382661df97cdfa48fede1ad2b767b95268bc810e11637234bb353085e37c32c7"]}'
+
+# 트랜젝션 확인 bob-index
+# peer chaincode query -C mychannel -n matter-tunnel -c '{"function":"GetTransaction","Args":["04992a9a2eb063fc4cd45c5b67a1aecebcb87ea8358de73eef0ccf7d58c3c3ff7e382661df97cdfa48fede1ad2b767b95268bc810e11637234bb353085e37c32c7","3"]}'
+
+# 모든 트랜젝션 출력
+# peer chaincode query -C mychannel -n matter-tunnel -c '{"function":"GetAllTransactions","Args":[]}'
 
 # 포맷
 # peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C mychannel -n matter-tunnel --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" -c '{"function":"","Args":["",""]}'
